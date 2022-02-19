8526eeae70586876ec1fee55429727a90459df36
Sam Worthington <sjw233@bath.ac.uk>
2022-02-19 23:40:39 +0000

    Setting up dictionaries

--- wordle_ai.py
+++ wordle_ai.py
@@ -1,3 +1,12 @@
+def get_wordle_guesses():
+    words = []
+
+    file = open("wordle_guesses.txt", "r")
+
+    for word in file:
+        words.append(word.strip())
+    return words
+
 def get_wordle_answers():
     words = []
 
@@ -8,11 +17,42 @@ def get_wordle_answers():
     return words
 
 def run():
-    first_word = ["slate"]
-    print("Please enter the word:",first_word[0].upper())
+    guesses = get_wordle_guesses()
+    answers = get_wordle_answers()
     
+
+    evaluation_of_answers = {}
+    min_wordcount = 1e6
+    chosen_word = ""
+
+    #6 attempts to guess the word
+    for guess in range(6):
+        if guess > 0:
+            potential_words = guesses
+        else:
+            potential_words = ["slate"]
+
+        for guess in guesses:
+            temp_evaluation_dict = {}
+
+        
+
+    if len(answers) == 1:
+        print("Answer should be:",answers)
+
+
+def give_feedback():
     user_feedback = input("Feedback: ")
 
+    if len(user_feedback) != 5:
+        print("5 digits only. Try again\n")
+        give_feedback()
+    else:
+        feedback = []
+        for i in range(5):
+            feedback.append(user_feedback[i])
+
+    return feedback
 
 if __name__ == "__main__":
     print('''
@@ -22,6 +62,8 @@ in the following format:
 0 = nothing
 1 = yellow
 2 = green
+
+Please enter the word: SLATE
     ''')
 
     run()
